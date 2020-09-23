d275d93bc2e8a5881a18751e4b6a8c10dbb53cfc
Eden <1490306321@qq.com>
2020-09-23 23:03:28 +0800

    文章项目初始化

---
+++ tensquare_article/src/main/java/com/tensquare/article/ArticleApplication.java
@@ -0,0 +1,17 @@
+package com.tensquare.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO 类描述
+ *
+ * @author Eden
+ * @date 2020/9/23 22:54
+ */
+@SpringBootApplication
+public class ArticleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class, args);
+    }
+}
